0c68e0164afc0d1e6bd42c5ee3af38364fdcfa78
Ivan <talisantos23@hotmail.com>
2019-07-31 20:42:14 +0100

    avatar icon added

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,6 @@
 
 
             <v-content>
-                <p>
-                    <router-link to="/home">Home</router-link>
-                </p>
-                <p>
-                    <router-link to="/about">About</router-link>
-                </p>
                 <router-view></router-view>
             </v-content>
         </div>
--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,42 +1,30 @@
 <template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
     <div id="toolbar">
-            <v-card>
-                <v-toolbar
-                        color="blue"
-                        dark
-                        flat
-                >
-                    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-app-bar>
 
-                    <v-toolbar-title>newgFit</v-toolbar-title>
+            <v-toolbar-title>newgFit</v-toolbar-title>
 
+            <v-spacer></v-spacer>
 
-                    <template v-slot:extension>
-                        <v-tabs
-                                v-model="tab"
-                                align-with-title
-                                background-color="transparent"
-                        >
-                            <v-tabs-slider color="yellow"></v-tabs-slider>
+            <template>
+                <v-toolbar-items>
+                    <v-btn text to="/home">Home</v-btn>
+                    <v-btn text to="/about">About</v-btn>
 
-                            <v-tab v-for="item in items" :key="item">
-                                {{ item }}
-                            </v-tab>
-                        </v-tabs>
-                    </template>
-                </v-toolbar>
-
-                <v-tabs-items v-model="tab">
-                    <v-tab-item
-                            v-for="item in items"
-                            :key="item"
+                    <v-avatar id="avatar"
+                              size="52px"
+                              :color="color"
+                              :tile="tile"
                     >
-                        <v-card flat>
-                            <v-card-text v-text="text"></v-card-text>
-                        </v-card>
-                    </v-tab-item>
-                </v-tabs-items>
-            </v-card>
+                        <img
+                                src="https://avatars0.githubusercontent.com/u/9064066?v=4&s=460"
+                                alt="Avatar"
+                        >
+                    </v-avatar>
+                </v-toolbar-items>
+            </template>
+        </v-app-bar>
+
     </div>
 </template>
 
@@ -44,13 +32,25 @@
 <script>
     export default {
 
-    data: () => ({
-        tab: null,
-         items: [
+        data: () => ({
+            tab: null,
+            items: [
                 'Home', 'About', 'videos', 'images', 'news',
             ],
             text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            tile: false,
+            color: 'grey lighten-4',
+            img: true
         }),
     };
 
 </script>
+
+
+<style>
+    #avatar {
+        align-self: center;
+        margin-left: 15px;
+        margin-right: 0px;
+    }
+</style>
