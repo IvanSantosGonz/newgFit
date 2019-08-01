4bc683e901d094e1be10412f21806d645d68e65c
Ivan <talisantos23@hotmail.com>
2019-08-01 13:52:23 +0100

    firebase config

--- src/main.js
+++ src/main.js
@@ -1,13 +1,32 @@
 import Vue from 'vue'
-import router from './router'
+import Router from './router'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import Vuetify from './plugins/vuetify';
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
+import VueFire from 'vuefire'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import firebaseConfig from '../.firebaseConfig.json'
 
-const app = new Vue({
+var fireConfig = {
+    apiKey: firebaseConfig.APIKEY,
+    authDomain: firebaseConfig.AUTHDOMAIN,
+    databaseURL: firebaseConfig.DATABASEURL,
+    projectId: firebaseConfig.PROJECTID,
+    storageBucket: firebaseConfig.STORAGEBUCKET,
+    messagingSenderId: firebaseConfig.MESSAGINGSENDERID,
+    appId: firebaseConfig.APPID
+};
+
+firebase.initializeApp(fireConfig);
+
+
+new Vue({
     el: '#app',
-    router: router,
-    vuetify,
+    router: Router,
+    Vuetify,
+    VueFire,
+    firebase,
     render: h => h(App)
 });
