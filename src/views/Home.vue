8489c6cd19cd452682146c9f26068a7876dc9075
Ivan <talisantos23@hotmail.com>
2019-08-14 13:30:01 +0100

    get last 7 days steps from google fit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -35,6 +35,7 @@
         data: () => ({
             result: "",
             steps: 0,
+            weekSteps: {},
         }),
         components: {
             BarDataCard,
@@ -44,7 +45,7 @@
         },
         mounted() {
             this.$eventBus.$on('emitAuthToken', payload =>
-                this.getTodaySteps(payload)
+                this.get7DaysSteps(payload)
             )
         },
         methods: {
@@ -61,7 +62,7 @@
                 });
             },
 
-            getTodaySteps(token) {
+            get7DaysSteps(token) {
                 axios({
                     headers: {
                         Authorization: 'Bearer ' + token //the token is a variable which holds the token
@@ -71,12 +72,29 @@
                             "dataSourceId": "derived:com.google.step_count.delta:com.google.android.gms:estimated_steps"
                         }],
                         "bucketByTime": {"durationMillis": 86400000},//24 hours
-                        "startTimeMillis": this.getStartOfDayMillis(),
+                        "startTimeMillis": this.get7DaysBeforeTodayMillis(),
                         "endTimeMillis": new Date().getTime()
                     }
                 }).then(response => {
+                        //parseInt(response.data.bucket[0].dataset[0].point[0].value[0].intVal)
+                        var weekday = new Array(7);
+                        weekday[0] = "Su";
+                        weekday[1] = "M";
+                        weekday[2] = "Tu";
+                        weekday[3] = "W";
+                        weekday[4] = "Th";
+                        weekday[5] = "F";
+                        weekday[6] = "Sa";
+
+                        for (var i = 0; i <= response.data.bucket.length - 1; i++) {
+                            var date = new Date()
+                            console.log("today is ", date.getDay())
+                            date.setDate(date.getDate() + i + 1 )
+                            console.log("day = ", date.getDay())
+                            this.weekSteps[weekday[date.getDay()]] = parseInt(response.data.bucket[i].dataset[0].point[0].value[0].intVal)
+                        }
                         console.log(response);
-                        this.steps = parseInt(response.data.bucket[0].dataset[0].point[0].value[0].intVal)
+                        console.log(this.weekSteps);
                     },
                     error => {
                         console.log(error);
@@ -84,6 +102,12 @@
             },
             getStartOfDayMillis() {
                 var now = new Date();
+                now.setUTCHours(0, 0, 0, 0);
+                return now.getTime()
+            },
+            get7DaysBeforeTodayMillis() {
+                var now = new Date();
+                now.setDate(now.getDate() - 6)
                 now.setUTCHours(0, 0, 0, 0);
                 return now.getTime()
             },
